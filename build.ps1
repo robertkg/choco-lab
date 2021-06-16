3fde096b59bf009417804d7a1430a0d0caaf2d89
robertkg <rgranmo@gmail.com>
2021-06-16 15:37:34 +0200

    Update build script step push packages

--- build.ps1
+++ build.ps1
@@ -1,13 +1,35 @@
 $ErrorActionPreference = 'Stop'
 
-Write-Output '----------- BEGIN DOCKER COMPOSE -----------'
+Write-Output '----------- DOCKER COMPOSE -----------'
 docker-compose up -d --build --force-recreate
 
 Write-Output '----------- CHOCOSERVER MANIFEST -----------'
 bolt apply .\manifests\chocolateyserver.pp -t chocoserver
 
 Write-Output '----------- PUSH PACKAGES -----------'
-& .\files\Push-ChocolateyPackage.ps1
+. .\files\Get-ChocolateyPackage.ps1
+. .\files\Push-ChocolateyPackage.ps1
+
+$packages = @(
+    'chocolatey-core.extension'
+    'git'
+    'git.install'
+    'nodejs-lts'
+    'nodepadplusplus'
+    'notepadplusplus.install'
+    'DotNet4.5.2'
+    'vscode'
+    'vscode.install'
+    'KB2919355'
+    'KB2919442'
+    'sql-server-management-studio'
+)
+
+$packages | ForEach-Object {
+    Get-ChocolateyPackage -Name $_
+}
+
+Push-ChocolateyPackage 'C:\Temp\*.nupkg' -Confirm:$false
 
 Write-Output '----------- CLIENT MANIFEST -----------'
 bolt apply .\manifests\client.pp -t client
